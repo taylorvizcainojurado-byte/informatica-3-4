79a88d7f627d6741c7a9d832b10ea8648fd0ffab
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-04-21 21:41:02 +0000

    naming variables

--- naming-variables.c
+++ naming-variables.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    int number;
-    int testvalue;
-    int example_1;
-    float taylor;
+    int number;// take away the !
+    int testvalue;// take away the space
+    int example_1;// put the 1 after example_
+    float taylor;// you cannot use int
     printf("Variables created succesfully.\n");
 }
