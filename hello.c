c10b63a40e6b02713a3957fcaf3c0e78dee8ac71
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-04-24 14:48:15 +0000

    hello advanced

--- hello.c
+++ hello.c
@@ -2,10 +2,12 @@
 
 int main(void)
 {
+    //ask user for name
     char name[50];
     printf("Enter a User...\n");
     scanf("%s", &name);
     printf("Hello, %s\n", name);
+    //ask user for fav color
      char color[50];
     printf("Whats your favorite color?\n");
     scanf("%s", &color);
