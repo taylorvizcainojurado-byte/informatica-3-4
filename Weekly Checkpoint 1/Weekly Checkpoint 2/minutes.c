6ff82ad3b3daa140672c3bbd941e5f92b33dc636
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-04-29 17:27:36 +0000

    minutes to hours

---
+++ Weekly Checkpoint 1/Weekly Checkpoint 2/minutes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    int mov_run_t;
+    int start_t_h;
+    int start_t_m;
+    int end_t_h;
+    int end_t_m;
+    printf("Movie Length in minutes=");
+    scanf("%d",&mov_run_t);
+    printf("What is the starting time=");
+    scanf("%d""%d",&start_t_h,&start_t_m);
+    int min_t = mov_run_t/60;
+    int min_t_m = mov_run_t%60;
+    printf("The movie is %d hours and %d minutes long", min_t, min_t_m);
+    end_t_h = ;
+    end_t_m = move_run_t%60;
+}
--- minutes.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int m;
-    int h = m/60;
-    printf("Start Time=");
-}
