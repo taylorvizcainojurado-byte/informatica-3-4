9918bc368ce3fa1f7843994326901d4cd8ebc961
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-04-30 00:57:49 +0000

    movie time

--- weeklycheckpoint2/minutes.c
+++ weeklycheckpoint2/minutes.c
@@ -6,14 +6,16 @@ int main(void)
     int start_t_m;
     int end_t_h;
     int end_t_m;
-    printf("Movie Length in minutes=");
+    printf("Movie Length in minutes: ");
     scanf("%d",&mov_run_t);
-    printf("What is the starting time=");
-    scanf("%d""%d",&start_t_h,&start_t_m);
-    int min_t = mov_run_t/60;
+    printf("What is the starting time: ");
+    scanf("%d:%d",&start_t_h,&start_t_m);
+    int min_t_h = mov_run_t/60;
     int min_t_m = mov_run_t%60;
-    printf("The movie is %d hours and %d minutes long\n", min_t, min_t_m);
-    end_t_h = 60;
-    end_t_m = mov_run_t%60;
-    printf("End Time: %d\n", end_t_m);
+    printf("The movie is %d hours and %d minutes long\n", min_t_h, min_t_m);
+
+    end_t_h = mov_run_t/60+start_t_h;
+    end_t_m = mov_run_t%60+start_t_m;
+
+    printf("End Time: %d:%d \n", end_t_h, end_t_m);
 }
